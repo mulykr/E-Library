f2b497d2333b28c1432287747f4248e3155a2269
Roman Mulyk <work.mulykr@gmail.com>
2019-04-04 20:13:33 +0300

    Image utility created

---
+++ LiBook.Utilities/Images/ImageTool.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+
+namespace LiBook.Utilities.Images
+{
+    public class ImageTool
+    {
+        public static Image Resize(Image image, int width, int height)
+        {
+            var newImage = new Bitmap(width, height);
+            using (var g = Graphics.FromImage(newImage))
+            {
+                g.DrawImage(image, 0, 0, width, height);
+            }
+
+            return newImage;
+        }
+
+        public static Image CropMaxSquare(Image image)
+        {
+            var width = image.Width > image.Height ? image.Height : image.Width;
+            var height = width;
+
+            var newImage = new Bitmap(width, height);
+            using (var g = Graphics.FromImage(newImage))
+                g.DrawImage(
+                    image,
+                    new Rectangle(0, 0, width, height),
+                    new Rectangle(0, 0, width, height),
+                    GraphicsUnit.Pixel
+                );
+            return newImage;
+        }
+    }
+}
