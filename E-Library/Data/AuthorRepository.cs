128cf9b9e41f0b45ba9ad6ad617797b9ac306af3
VolodymyrVerm <volodymyr33929@gmail.com>
2019-04-05 12:38:50 +0300

    CreateAuthorRepository

---
+++ E-Library/Data/AuthorRepository.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using E_Library.Data;
+using E_Library.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiBook.Data
+{
+    public class AuthorRepository : IRepository<Author>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthorRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _disposed = false;
+        }
+
+        public IEnumerable<Author> GetList()
+        {
+            return _context.Authors;
+        }
+
+        public Author Get(int id)
+        {
+            return _context.Authors
+                .AsNoTracking()
+                .Include(item => item.AuthorsBooks)
+                .ThenInclude(item => item.Book)
+                .FirstOrDefault(item => item.Id == id);
+        }
+
+        public IEnumerable<Author> Get(Expression<Func<Author, bool>> filter = null, Func<IQueryable<Author>, IOrderedQueryable<Author>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<Author> query = _context.Authors;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public void Create(Author item)
+        {
+            _context.Authors.Add(item);
+        }
+
+        public void Update(Author item)
+        {
+            _context.Authors.Update(item);
+        }
+
+        public void Delete(int id)
+        {
+            var author = _context.Authors.Find(id);
+            if (author != null)
+                _context.Authors.Remove(author);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        #region Dispose pattern
+
+        private bool _disposed;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
