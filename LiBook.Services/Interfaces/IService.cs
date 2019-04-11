21e1aee25c996af3e33721be0f47ee6ccce3885a
Tanianatania <tatiana.gutiy@gmail.com>
2019-04-11 14:37:15 +0300

    Add IService interface

---
+++ LiBook.Services/Interfaces/IService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace LiBook.Services.Interfaces
+{
+    public interface IService<T> : IDisposable where T : class
+    {
+        IEnumerable<T> GetList();
+        T Get(int id);
+
+        IEnumerable<T> Get(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = "");
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+        void Save();
+        
+    }
+}
