e7f01439047615d8fad2f218991e5dcc3e640d12
VolodymyrVerm <volodymyr33929@gmail.com>
2019-04-11 14:34:02 +0300

    Add BookViewModel

---
+++ LiBook.Models/DTO/BookViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace LiBook.Models.DTO
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public ICollection<AuthorBookViewModel> AuthorsBooks { get; set; }
+
+    }
+}
