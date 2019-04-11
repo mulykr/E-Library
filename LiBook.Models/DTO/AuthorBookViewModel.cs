15e03b5545d1d59b1216e9aa97dc25579adb63b1
Tanianatania <tatiana.gutiy@gmail.com>
2019-04-11 14:36:29 +0300

    Add AuthorBookViewModel

---
+++ LiBook.Models/DTO/AuthorBookViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiBook.Models.DTO
+{
+    public class AuthorBookViewModel
+    {
+        public int Id { get; set; }
+
+        public string BookId { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public BookViewModel Book { get; set; }
+
+        public AuthorViewModel Author { get; set; }
+    }
+}
