eef609c4f688dbc54598f5ed8f39d16ecb1601b7
VolodymyrVerm <volodymyr33929@gmail.com>
2019-04-11 14:34:50 +0300

    Add AuthorViewModel

---
+++ LiBook.Models/DTO/AuthorViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiBook.Models.DTO
+{
+    public class AuthorViewModel
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string Biography { get; set; }
+
+        public ICollection<AuthorBookViewModel> AuthorsBooks { get; set; }
+    }
+}
