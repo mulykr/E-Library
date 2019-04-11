6c48c05c9f129d58296c81f857223e57acc0a8e3
Roman Mulyk <work.mulykr@gmail.com>
2019-04-11 22:35:24 +0300

    Edit updated: file choosing

--- E-Library/wwwroot/js/site.js
+++ E-Library/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function readURL(input, targetId) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#'+targetId)
+                .attr('src', e.target.result);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
