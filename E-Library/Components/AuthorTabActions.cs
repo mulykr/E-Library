66bd148c2927cadb7ef9bd6c40358c3eefe87a72
Roman Mulyk <work.mulykr@epam.com>
2019-04-23 12:20:01 +0300

    Tab buttons

---
+++ E-Library/Components/AuthorTabActions.cs
@@ -0,0 +1,25 @@
+﻿using LiBook.Models;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace LiBook.Components
+{
+    public class AuthorTabActions : ViewComponent
+    {
+        public IViewComponentResult Invoke(AuthorViewModel model)
+        {
+            var isAdmin = User != null && User.IsInRole("Admin");
+            if (isAdmin)
+            {
+                return new HtmlContentViewComponentResult(
+                    new HtmlString($"<a href=\"/authors/details/{model.Id}\" class=\"btn btn-success\">Open</a> " +
+                                   $"<a href=\"/authors/edit/{model.Id}\" class=\"btn btn-info\">Edit</a> " +
+                                   $"<a href=\"/authors/delete/{model.Id}\" class=\"btn btn-danger\">Remove</a>"));
+            }
+
+            return new HtmlContentViewComponentResult(
+                new HtmlString($"<a href=\"/authors/details/{model.Id}\" class=\"btn btn-success\">Open</a>"));
+        }
+    }
+}
