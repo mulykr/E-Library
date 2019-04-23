2ca1b3dc1f0f7a64093d1f3aa207c4b3d4a13e17
Tatianatania <tatiana.gutiy@gmail.com>
2019-04-23 15:27:41 +0300

    Add Comment model

---
+++ LiBook.Data/Entities/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiBook.Data.Entities
+{
+    public class Comment
+    {
+        public string Id { get; set; }
+
+        public string BookId { get; set; }
+
+        public Book Book { get; set; }
+
+        public string UserId { get; set; }
+
+        public UserProfile User { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime TimaStamp { get; set; }
+    }
+}
