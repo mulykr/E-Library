e1ae079096906ba8a591cd931fa1f34c6101dad4
VolodymyrVerm <volodymyr339292@gmail.com>
2019-04-23 15:34:21 +0300

    Implemented CommentService

---
+++ LiBook.Services/CommentService.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using LiBook.Data.Entities;
+using LiBook.Data.Interfaces;
+using LiBook.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LiBook.Services.Interfaces
+{
+    public class CommentService:ICommentService
+    {
+        private readonly IRepository<Comment> _repository;
+        private readonly IMapper _mapper;
+
+        public CommentService(IRepository<Comment> repository,
+            IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public void Dispose()
+        {
+            _repository.Dispose();
+        }
+
+        public CommentDTO Get(string id)
+        {
+            return _mapper.Map<Comment, CommentDTO>(_repository.Get(id));
+        }
+
+        public IEnumerable<CommentDTO> GetByBook(BookDto book)
+        {
+            return _repository
+                .Get(i => i.BookId == book.Id)
+                .Select(i=>_mapper.Map<Comment, CommentDTO>(i));
+        }
+
+        public void AddComment(CommentDTO CommentDto)
+        {
+            var item = _mapper.Map<CommentDTO, Comment>(CommentDto);
+            item.TimaStamp = DateTime.Now;
+            _repository.Create(item);
+            _repository.Save();
+        }
+
+        public void DeleteComment(CommentDTO CommentDto)
+        {
+            var item = _repository.Get(i => i.BookId == CommentDto.BookId && i.UserId == CommentDto.UserId).First();
+            if(item!=null)
+            {
+                _repository.Delete(item.Id);
+                _repository.Save();
+
+            }
+        }
+
+    }
+}
