994062ef0817cba96000b8efee5a7b067881dec5
VolodymyrVerm <volodymyr339292@gmail.com>
2019-04-23 15:33:54 +0300

    Add ICommentService

---
+++ LiBook.Services/Interfaces/ICommentService.cs
@@ -0,0 +1,16 @@
+﻿using LiBook.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace LiBook.Services.Interfaces
+{
+    public interface ICommentService: IDisposable
+    {
+        CommentDTO Get(string id);
+        IEnumerable<CommentDTO> GetByBook(BookDto book);
+        void AddComment(CommentDTO CommentDto);
+        void DeleteComment(CommentDTO CommentDto);
+    }
+}
