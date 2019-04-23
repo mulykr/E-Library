b24a0876807d04526eed9ef33e8c8af262f12ac2
Tatianatania <tatiana.gutiy@gmail.com>
2019-04-23 15:29:30 +0300

    Create CommentViewModel

---
+++ E-Library/Models/CommentViewModel.cs
@@ -0,0 +1,23 @@
+﻿using LiBook.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiBook.Models
+{
+    public class CommentViewModel
+    {
+        public string BookId { get; set; }
+
+        public Book Book { get; set; }
+
+        public string UserId { get; set; }
+
+        public UserProfile User { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime TimaStamp { get; set; }
+    }
+}
