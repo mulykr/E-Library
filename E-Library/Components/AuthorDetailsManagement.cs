ef24586c88e37a9a1d1dee05e33aa41f766b8ac4
AlexMedyk <sanyamedyk@gmail.com>
2019-04-24 17:21:32 +0300

    Hide buttons "Edit"
    Add BookDetailsManagement and AuthorsDetailsManagement to hide from users button "Edit".

---
+++ E-Library/Components/AuthorDetailsManagement.cs
@@ -0,0 +1,24 @@
+﻿using LiBook.Models;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace LiBook.Components
+{
+    public class AuthorDetailsManagement: ViewComponent
+    {
+        public IViewComponentResult Invoke(AuthorViewModel model)
+        {
+            var isAdmin = User != null && User.IsInRole("Admin");
+            if (isAdmin)
+            {
+                return new HtmlContentViewComponentResult(
+                    new HtmlString($"<a href=\"/authors/Index/{model.Id}\" class=\"btn btn-info\">Back To List</a> " +
+                    $"<a href=\"/authors/Edit/{model.Id}\" class=\"btn btn-info\">Edit</a> "));
+            }
+
+            return new HtmlContentViewComponentResult(
+                new HtmlString($"<a href=\"/authors/Index/{model.Id}\" class=\"btn btn-success\">Back To List</a>"));
+        }
+    }
+}
---
+++ E-Library/Components/BookDetailsManagement.cs
@@ -0,0 +1,24 @@
+﻿using LiBook.Models;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace LiBook.Components
+{
+    public class BookDetailsManagement: ViewComponent
+    {
+        public IViewComponentResult Invoke(BookViewModel model)
+        {
+            var isAdmin = User != null && User.IsInRole("Admin");
+            if (isAdmin)
+            {
+                return new HtmlContentViewComponentResult(
+                    new HtmlString($"<a href=\"/books/Index/{model.Id}\" class=\"btn btn-info\">Back To List</a> " +
+                    $"<a href=\"/books/Edit/{model.Id}\" class=\"btn btn-info\">Edit</a> "));
+            }
+
+            return new HtmlContentViewComponentResult(
+                new HtmlString($"<a href=\"/books/Index/{model.Id}\" class=\"btn btn-success\">Back To List</a>"));
+        }
+    }
+}
