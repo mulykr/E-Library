aa501d346eb3cf7e192f82ec150d334d8f382413
VolodymyrVerm <volodymyr339292@gmail.com>
2019-05-09 14:55:32 +0300

    Create ISearchService

---
+++ LiBook.Services/Interfaces/ISearchService.cs
@@ -0,0 +1,13 @@
+﻿using LiBook.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiBook.Services.Interfaces
+{
+    public interface ISearchService
+    {
+        IEnumerable<BookDto> SearchBook(string key);
+        IEnumerable<AuthorDto> SearchAuthor(string key);
+    }
+}
